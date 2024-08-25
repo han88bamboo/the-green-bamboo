6162db1ae110bf0dfa1bc9c8c9b801001998c035
han88bamboo <han@88bamboo.co>
2024-08-25 17:17:22 +0800

    tzh

--- backend/scripts/editModRequests.py
+++ backend/scripts/editModRequests.py
@@ -1,11 +1,24 @@
 # Port: 5101
 # Routes: /submitModRequest (POST), /updateModRequest (POST)
+
+# [OLD] TO BE DELETED FOR POSTGRES:
+# ------------------------------------------------------
+##
+# ======================================================
+
+# [NEW] TO BE ADDED FOR POSTGRES:
+# ------------------------------------------------------
+##
+# ======================================================
+
 # -----------------------------------------------------------------------------------------
 
 import os
 from flask import Blueprint, g, request, jsonify
+# [OLD] TO BE DELETED FOR POSTGRES:
+# ------------------------------------------------------
 from bson.objectid import ObjectId
-
+# ======================================================
 file_name = os.path.basename(__file__)
 blueprint = Blueprint(file_name[:-3], __name__)
 
@@ -13,6 +26,7 @@ blueprint = Blueprint(file_name[:-3], __name__)
 # [POST] Submit mod request
 # - Submit mod request with new details
 # - Possible return codes: 201 (Updated), 500 (Error during update)
+
 @blueprint.route('/submitModRequest', methods=['POST'])
 def submitModRequest():
     db = g.db
@@ -22,6 +36,8 @@ def submitModRequest():
     drinkType = data['drinkType']
     modDesc = data['modDesc']
 
+# [OLD] TO BE DELETED FOR POSTGRES:
+# ------------------------------------------------------
     try: 
         submitReq = db.modRequests.insert_one({
             'userID': ObjectId(userID),
@@ -41,6 +57,33 @@ def submitModRequest():
                 }
             }
         ), 201
+# ======================================================    
+# [NEW] TO BE ADDED FOR POSTGRES:
+# ------------------------------------------------------
+    # try: 
+    #     cur = db.cursor()
+    #     cur.execute(
+    #         """
+    #         INSERT INTO modRequests (userID, drinkType, modDesc, reviewStatus)
+    #         VALUES (%s, %s, %s, %s)
+    #         """,
+    #         (userID, drinkType, modDesc, True)
+    #     )
+    #     db.commit()
+
+    #     return jsonify(
+    #         {   
+    #             "code": 201,
+    #             "data": {
+    #                 "userID": userID,
+    #                 "drinkType": drinkType,
+    #                 "modDesc": modDesc, 
+    #                 "reviewStatus": True
+    #             }
+    #         }
+    #     ), 201
+# ======================================================
+
     except Exception as e:
         print(str(e))
         return jsonify(
@@ -70,9 +113,26 @@ def updateModRequest():
     requestID = data['requestID']
     reviewStatus = data['reviewStatus']
 
+
+# [OLD] TO BE DELETED FOR POSTGRES:
+# ------------------------------------------------------
     try: 
         updateReviewStatus = db.modRequests.update_one({'_id': ObjectId(requestID)}, {'$set': {'reviewStatus': reviewStatus}})
+# ======================================================
+# [NEW] TO BE ADDED FOR POSTGRES:
+# ------------------------------------------------------
+    # try:
+    #     cur.execute(
+    #         """
+    #         UPDATE modRequests
+    #         SET reviewStatus = %s
+    #         WHERE id = %s
+    #         """,
+    #         (reviewStatus, requestID)
+    #     )
+    #     db.commit()
 
+# ======================================================
         return jsonify(
             {   
                 "code": 201,
@@ -82,6 +142,7 @@ def updateModRequest():
                 }
             }
         ), 201
+    
     except Exception as e:
         print(str(e))
         return jsonify(
