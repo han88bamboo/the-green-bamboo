0e3ee2d057f769e499df00bd8b25d095c17fb1d7
d.chien <dangchien1910@gmail.com>
2024-10-27 15:45:46 +0800

    wip

--- backend/app.py
+++ backend/app.py
@@ -45,8 +45,11 @@ def get_db_connection():
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
 app.config["MAIL_PORT"] = os.getenv("MAIL_PORT")
 app.config["MAIL_USE_TLS"] = True
+app.config['MAIL_USE_SSL'] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_DEFAULT_SENDER'] = 'noreply@drink-x.com'
+
 mail = Mail(app)
 
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
