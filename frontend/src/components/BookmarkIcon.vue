cb3e3ee3b5fcc955c38eccb08f90fcec9031e603
Cheah Cheng Pong <cpcheah.2021@scis.smu.edu.sg>
2024-12-13 19:25:13 +0800

    feat: show 'want to try' buttons even when user not logged in
    Refactored frontend to allow public users to click on buttons even when they are not logged in on the bottle listing page (bottle listing file in producers folder)
    
    Added comments to debug why producers updates are not showing on the home page (bottle listings file in users folder)

--- frontend/src/components/BookmarkIcon.vue
+++ frontend/src/components/BookmarkIcon.vue
@@ -9,14 +9,25 @@ handleIconClick(data) {
  -->
 
 <template>
-    <svg v-if="checkBookmarkStatus(listing.id)" xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" fill="currentColor" class="bi bi-bookmark-fill" :class="{ 'overlay-icon': overlay }" viewBox="0 0 16 16"
-        data-bs-toggle="modal" data-bs-target="#bookmarkModal" @click="iconClicked">
-        <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
-    </svg>
-    <svg v-else xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" fill="currentColor" class="bi bi-bookmark" :class="{ 'overlay-icon': overlay }" viewBox="0 0 16 16"
-        data-bs-toggle="modal" data-bs-target="#bookmarkModal" @click="iconClicked">
-        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
-    </svg>
+    <!-- CP edits: added v-if div to ensure when public user click on bookmark icon, it does not throw out an error-->
+    <div v-if="user">
+        <svg v-if="checkBookmarkStatus(listing.id)" xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" fill="currentColor" class="bi bi-bookmark-fill" :class="{ 'overlay-icon': overlay }" viewBox="0 0 16 16"
+            data-bs-toggle="modal" data-bs-target="#bookmarkModal" @click="iconClicked">
+            <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+        </svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" fill="currentColor" class="bi bi-bookmark" :class="{ 'overlay-icon': overlay }" viewBox="0 0 16 16"
+            data-bs-toggle="modal" data-bs-target="#bookmarkModal" @click="iconClicked">
+            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+        </svg>
+    </div>
+
+    <div v-else>
+        <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" fill="currentColor" class="bi bi-bookmark" :class="{ 'overlay-icon': overlay }" viewBox="0 0 16 16"
+            @click="iconClicked">
+            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+        </svg>
+    </div>
+    
 </template>
 
 <script>
@@ -51,9 +62,17 @@ handleIconClick(data) {
                     }
                 }
             },
-            iconClicked() {
-                this.$emit('icon-clicked', this.listing.id);
-            }
+            iconClicked() {           
+                if (this.user) {
+                    this.$emit('icon-clicked', this.listing.id);
+                }
+                else {
+                    this.$emit('icon-clicked', 'login');
+                }
+                
+            },
+
+            
 
         }
     }
