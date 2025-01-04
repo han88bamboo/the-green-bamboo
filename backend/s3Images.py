8f8ed69f90bfe48bf3af48d594bebfd07ac460db
Cheah Cheng Pong <40486171+cpong1@users.noreply.github.com>
2025-01-04 11:02:19 +0800

    update s3Images.py with local / deployment comments

--- backend/s3Images.py
+++ backend/s3Images.py
@@ -10,17 +10,21 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# bucket_name = 'tf-drinkx-prod-fe-images'
-# region='ap-southeast-1'
-bucket_name = 'drinkximages'
-region='us-east-1'
+# For deployment
+bucket_name = 'tf-drinkx-prod-fe-images'
+region='ap-southeast-1'
+
+# For local development
+# bucket_name = 'drinkximages'
+# region='us-east-1'
 
 def uploadBase64ImageToS3(base64_string):
 
-    # credentials = { 
-    #     'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
-    #     'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
-    # }
+    # For deployment
+    credentials = { 
+        'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
+        'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
+    }
 
 
     # Decode the base64 string
@@ -31,9 +35,11 @@ def uploadBase64ImageToS3(base64_string):
         return base64_string
 
     # Initialize a session using Amazon S3
-    #Create an S3 client using boto3, which will automatically use the credentials provided by the IAM role associated with the ECS task.
-    s3 = boto3.client('s3')
-    # s3 = boto3.client('s3', region_name=region, **credentials)
+    # Create an S3 client using boto3, which will automatically use the credentials provided by the IAM role associated with the ECS task.
+    # s3 = boto3.client('s3')
+
+    # For deployment
+    s3 = boto3.client('s3', region_name=region, **credentials)
 
     object_key = f'{uuid.uuid4()}.jpg'
     try:
@@ -68,10 +74,11 @@ def uploadURLtoS3(url):
 
 def deleteImageFromS3(url):
 
-    # credentials = { 
-    #     'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
-    #     'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
-    # }
+    # For deployment
+    credentials = { 
+        'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
+        'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
+    }
 
     # Get the key from the url by stripping https://testbucketdrinkx.s3.amazonaws.com/xxxx
     # object_key = url[48:]
@@ -87,7 +94,11 @@ def deleteImageFromS3(url):
         object_key = 'None'
 
     # Initialize a session using Amazon S3
-    # s3 = boto3.client('s3', region_name=region, **credentials)
+
+    # For deployment
+    3 = boto3.client('s3', region_name=region, **credentials)
+
+    # For local development
     s3 = boto3.client('s3')
 
     try:
