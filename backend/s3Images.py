f56208fa4170a9b8a46237492e9800517aa6aff5
Cheah Cheng Pong <40486171+cpong1@users.noreply.github.com>
2025-01-12 12:42:16 +0800

    updated s3Images.py with deployment comments

--- backend/s3Images.py
+++ backend/s3Images.py
@@ -10,17 +10,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# For deployment
+# For deployment (comment out for local launch)
 # bucket_name = 'tf-drinkx-prod-fe-images'
 # region='ap-southeast-1'
 
-# For local development
+# For local development (comment out before deployment)
 bucket_name = 'drinkximages'
 region='us-east-1'
 
 def uploadBase64ImageToS3(base64_string):
 
-    # For local development
+    # For local development (comment out before deployment)
     credentials = { 
         'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
         'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -36,10 +36,10 @@ def uploadBase64ImageToS3(base64_string):
 
     # Initialize a session using Amazon S3
     # Create an S3 client using boto3, which will automatically use the credentials provided by the IAM role associated with the ECS task.
-    # For deployment
+    # For deployment (uncomment before deployment)
     # s3 = boto3.client('s3')
 
-    # For local development
+    # For local development (comment out before deployment)
     s3 = boto3.client('s3', region_name=region, **credentials)
 
     object_key = f'{uuid.uuid4()}.jpg'
@@ -75,7 +75,7 @@ def uploadURLtoS3(url):
 
 def deleteImageFromS3(url):
 
-    # For local development
+    # For local development (comment out before deployment)
     credentials = { 
         'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
         'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -96,10 +96,10 @@ def deleteImageFromS3(url):
 
     # Initialize a session using Amazon S3
 
-    # For local development
+    # For local development (comment out before deployment)
     s3 = boto3.client('s3', region_name=region, **credentials)
 
-    # For deployment
+    # For deployment (uncomment before deployment)
     # s3 = boto3.client('s3')
 
     try:
