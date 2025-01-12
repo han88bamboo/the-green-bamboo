ed59cd9b14db4d2afe621a083c2f09d31714011b
Cheah Cheng Pong <40486171+cpong1@users.noreply.github.com>
2025-01-12 14:44:44 +0800

    feat: updated tab icon

--- frontend/src/components/ClubSettings.vue
+++ frontend/src/components/ClubSettings.vue
@@ -113,7 +113,7 @@
                     <tr v-for="member in paginatedMembers" :key="member.id">
                         <!-- Column 1: Photo -->
                         <td>
-                            <img v-if="member.photo" :src="friends[id].photo" class="rounded-circle" style="width: 50px; height: 50px;" alt="profile-photo">
+                            <img v-if="member.photo" :src="member.photo" class="rounded-circle" style="width: 50px; height: 50px;" alt="profile-photo">
                             <svg v-else xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
