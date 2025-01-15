9718eab40e295c46d5ad66d8bc2195ae7efa19b9
Cheah Cheng Pong <40486171+cpong1@users.noreply.github.com>
2025-01-15 11:29:56 +0800

    update: updated files that requires commenting of codes for deployment
    Created a variable in the .env to remove the need to comment and uncomment codes in these 2 files

--- backend/s3Images.py
+++ backend/s3Images.py
@@ -10,21 +10,27 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# For deployment (comment out for local launch)
-# bucket_name = 'tf-drinkx-prod-fe-images'
-# region='ap-southeast-1'
+# Get the purpose variable from the environment
+purpose = os.getenv('PURPOSE')
+print(f"Purpose: {purpose}")
 
-# For local development (comment out before deployment)
-bucket_name = 'drinkximages'
-region='us-east-1'
+# For deployment (comment out for local launch)
+if purpose == 'production':
+    bucket_name = 'tf-drinkx-prod-fe-images'
+    region='ap-southeast-1'
+else:
+    bucket_name = 'drinkximages'
+    region='us-east-1'
+
 
 def uploadBase64ImageToS3(base64_string):
 
     # For local development (comment out before deployment)
-    credentials = { 
-        'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
-        'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
-    }
+    if purpose == 'development':
+        credentials = { 
+            'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
+            'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
+        }
 
 
     # Decode the base64 string
@@ -37,10 +43,10 @@ def uploadBase64ImageToS3(base64_string):
     # Initialize a session using Amazon S3
     # Create an S3 client using boto3, which will automatically use the credentials provided by the IAM role associated with the ECS task.
     # For deployment (uncomment before deployment)
-    # s3 = boto3.client('s3')
-
-    # For local development (comment out before deployment)
-    s3 = boto3.client('s3', region_name=region, **credentials)
+    if purpose == 'production':
+        s3 = boto3.client('s3')
+    else:
+        s3 = boto3.client('s3', region_name=region, **credentials)
 
     object_key = f'{uuid.uuid4()}.jpg'
     try:
@@ -76,10 +82,11 @@ def uploadURLtoS3(url):
 def deleteImageFromS3(url):
 
     # For local development (comment out before deployment)
-    credentials = { 
-        'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
-        'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
-    }
+    if purpose == 'development':
+        credentials = { 
+            'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
+            'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY')
+        }
 
     # Get the key from the url by stripping https://testbucketdrinkx.s3.amazonaws.com/xxxx
     # object_key = url[48:]
@@ -97,10 +104,10 @@ def deleteImageFromS3(url):
     # Initialize a session using Amazon S3
 
     # For local development (comment out before deployment)
-    s3 = boto3.client('s3', region_name=region, **credentials)
-
-    # For deployment (uncomment before deployment)
-    # s3 = boto3.client('s3')
+    if purpose == 'development':
+        s3 = boto3.client('s3', region_name=region, **credentials)
+    else:
+        s3 = boto3.client('s3')
 
     try:
         # Upload the image to S3
