478964e09ff3a63dc03c40f236dd9ef0bcfc6e59
TeckXuan <khooteckxuan@gmail.com>
2025-02-19 16:53:46 +0800

    Updated insert sql and routed navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -83,7 +83,7 @@
                         <li v-if="isAdmin"><router-link :to="'/admin/importListings'" class="dropdown-item">Import Listings</router-link></li>
                         
                         <div class="mobile-view-show">
-                            <li><router-link :to="'/'" class="dropdown-item">Explore</router-link></li>
+                            <li><router-link :to="'/explore'" class="dropdown-item">Explore</router-link></li>
                             <li><router-link :to="'/'" class="dropdown-item">Best Of</router-link></li>
                             <li><router-link :to="dashboardURL" class="dropdown-item">{{ dashboardWord }} Dashboard</router-link></li>
                             <li><span  @click="externalURL('https://88bamboo.co/')" class="dropdown-item">Latest News</span></li>
@@ -117,7 +117,7 @@
             </div>
             <div class="mobile-view-hide container-fluid align-items-center col-xxl-8 col-xl-9 col-lg-10 col-md-11 col-sm-12">
 
-                <router-link :to="'/'">
+                <router-link :to="'/explore'">
                     <button class="btn primary-btn border-0 fw-bold" type="button">
                         Explore
                     </button>
