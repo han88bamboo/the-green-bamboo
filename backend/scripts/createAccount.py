c7e991d60f27206abc75ea8ec09e0a204dc985b4
DyCiA13 <broccolicarrotz@gmail.com>
2025-02-22 21:32:57 +0800

    Fix(createAccount.py): Database connection

--- backend/scripts/createAccount.py
+++ backend/scripts/createAccount.py
@@ -145,13 +145,14 @@ def createAccount():
 @blueprint.route("/addPreferences/<username>", methods = ['POST'])
 def add_preferences(username):
     load_dotenv
-    conn = psycopg2.connect(
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST"),
-        port=os.getenv("POSTGRES_PORT")
-    )
+    # conn = psycopg2.connect(
+    #     dbname=os.getenv("POSTGRES_DB"),
+    #     user=os.getenv("POSTGRES_USER"),
+    #     password=os.getenv("POSTGRES_PASSWORD"),
+    #     host=os.getenv("POSTGRES_HOST"),
+    #     port=os.getenv("POSTGRES_PORT")
+    # )
+    conn = g.db
     cur = conn.cursor()
     rawAccount = request.get_json()
     cur.execute("""
