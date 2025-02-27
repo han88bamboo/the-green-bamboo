421a268573ee30ef984925104d107d3b1e06af1f
Cheah Cheng Pong <40486171+cpong1@users.noreply.github.com>
2025-02-27 21:08:46 +0800

    fix: fix sql scripts issue

--- database/postgresql/final/01-postgresql_data3.sql
+++ database/postgresql/final/01-postgresql_data3.sql
@@ -488,6 +488,26 @@ CREATE TABLE "clubMembers" (
     "joinStatus" BOOLEAN
 );
 
+-- ========= "clubInvites" =========
+CREATE TABLE "clubInvites" (
+    "id" SERIAL PRIMARY KEY,
+    "clubID" INTEGER REFERENCES "clubs"("id") ON DELETE SET NULL, -- [!] References clubs FK
+    "inviteeID" INTEGER,
+    "inviteeUserType" VARCHAR(255),
+    "inviterID" INTEGER,
+    "inviterUserType" VARCHAR(255),
+    "inviteDate" TIMESTAMP
+);
+
+-- ========= "clubRequests" =========
+CREATE TABLE "clubRequests" (
+    "id" SERIAL PRIMARY KEY,
+    "clubID" INTEGER REFERENCES "clubs"("id") ON DELETE SET NULL, -- [!] References clubs FK
+    "userID" INTEGER,
+    "userType" VARCHAR(255),
+    "requestDate" TIMESTAMP
+);
+
 -- ========= "clubPosts" =========
 CREATE TABLE "clubPosts" (
     "id" SERIAL PRIMARY KEY,
