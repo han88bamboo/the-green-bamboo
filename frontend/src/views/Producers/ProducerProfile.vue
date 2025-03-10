3bd09c68f547410b55780c8407c9242666232f1c
han88bamboo <han@88bamboo.co>
2025-03-10 21:58:58 +0800

    Revert "bug fix: fix producer profile error"
    This reverts commit ac8b520c3192f1dc9f494ec2a1c346fd708917ec.
    
    this is to revert to 24 Feb 2025

--- frontend/src/views/Producers/ProducerProfile.vue
+++ frontend/src/views/Producers/ProducerProfile.vue
@@ -2368,12 +2368,10 @@
                 // producer listings
                 // _id, listingName, producerID, bottler, originCountry, drinkType, typeCategory, age, abv, reviewLink, officialDesc, sourceLink, photo
                     try {
-                        const response = await this.$axios.get(`${process.env.VUE_APP_API_URL}/getData/getListingsByProducer/${this.producer_id}`);
+                        const response = await this.$axios.get(`${process.env.VUE_APP_API_URL}/getData/ByProducer/${this.producer_id}`);
                         this.listings = response.data;
-                        this.allDrinks = response.data;
-                        this.allDrinksCount = response.data.length
 
-                        // this.getAllDrinks()
+                        this.getAllDrinks()
                         this.getCountsByType()
                         this.getTotalCounts()
                         this.getMostDiscussed()
@@ -2464,12 +2462,12 @@
                 }
             },
 
-            // // get all drinks that a producer has
-            // async getAllDrinks() {
-            //     let allProducerDrinks = this.listings.filter(listing => listing.producerID == this.producer_id);
-            //     this.allDrinks = allProducerDrinks;
-            //     this.allDrinksCount = allProducerDrinks.length
-            // },
+            // get all drinks that a producer has
+            async getAllDrinks() {
+                let allProducerDrinks = this.listings.filter(listing => listing.producerID == this.producer_id);
+                this.allDrinks = allProducerDrinks;
+                this.allDrinksCount = allProducerDrinks.length
+            },
 
             // get all reviews that a producer has
             async getAllReviews() {
