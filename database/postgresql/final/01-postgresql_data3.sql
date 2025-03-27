1acf8f2b2895447ee1c4d62d59f994062d969be0
DyCiA13 <broccolicarrotz@gmail.com>
2025-03-27 21:28:37 +0800

    Fix(01-postgresql_data3.sql)
    Fixed the syntax error in creating users table

--- database/postgresql/final/01-postgresql_data3.sql
+++ database/postgresql/final/01-postgresql_data3.sql
@@ -228,7 +228,7 @@ CREATE TABLE "users" (
     "birthday" TIMESTAMP,
     "pin" VARCHAR(255),
     "choiceFlavours" TEXT[], -- SMU Group 3 added in "choiceFlavours"
-    "preferences" TEXT[] -- SMU Group 3 added in "preferences"
+    "preferences" TEXT[], -- SMU Group 3 added in "preferences"
     "grails" TEXT[], -- SMU Group 3 added in "grails"
     "rideOrDies" TEXT[], -- SMU Group 3 added in "rideOrDies"
     "goats" TEXT[] -- SMU Group 3 added in "goats"
