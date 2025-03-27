328b34949e3ad31eda7e4d4a2ad7e22c9488da03
DyCiA13 <broccolicarrotz@gmail.com>
2025-03-27 20:41:39 +0800

    Fix(LandingPageNavBar.vue): Added in tabs for For You Page
    Updated Component - LandingPageNavBar (LandingPageNavBar.vue)
    - To include the tab for "For You" page

--- frontend/src/components/LandingPageNavBar.vue
+++ frontend/src/components/LandingPageNavBar.vue
@@ -65,6 +65,8 @@
                         
                         <div class="mobile-view-show">
                             <li><router-link :to="'/explore'" class="dropdown-item">Explore</router-link></li>
+                            <!-- Added by SMU Group 3 -->
+                            <li><router-link :to="'/foryou'" class="dropdown-item">For You</router-link></li>
                             <li><router-link :to="'/explore'" class="dropdown-item">Best Of</router-link></li>
                             <li><router-link :to="dashboardURL" class="dropdown-item">{{ dashboardWord }} Dashboard</router-link></li>
                             <li><span  @click="externalURL('https://88bamboo.co/')" class="dropdown-item">Latest News</span></li>
@@ -98,7 +100,12 @@
                         Explore
                     </button>
                 </router-link>
-
+                <!-- Added by SMU Group 3 -->
+                <router-link :to="'/foryou'">
+                    <button class="btn primary-btn border-0 fw-bold" type="button">
+                        For You
+                    </button>
+                </router-link>
                 <router-link :to="'/explore'">
                     <button class="btn primary-btn border-0 fw-bold" type="button">
                         Best Of
