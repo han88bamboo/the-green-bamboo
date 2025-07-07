c389510f2beb6c433a543f950f1820e9dde61c1e
Cheah Cheng Pong <40486171+cpong1@users.noreply.github.com>
2025-07-07 23:07:47 +0800

    bug fix: increase default distance to make it place at the bottom on Where to try

--- frontend/src/views/Producers/BottleListings.vue
+++ frontend/src/views/Producers/BottleListings.vue
@@ -3027,7 +3027,7 @@ export default {
               console.error("Error in getDistance request:", error);
 
               // If there's an error, just add to nearestBars with no distance
-              venueDistances[venue.id] = 9999; // Default distance if API fails
+              venueDistances[venue.id] = 99999999; // Default distance if API fails
 
               this.venueDetails[venue.id] = {
                 distance: "NA",
