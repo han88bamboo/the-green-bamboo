cf0c3ce886d5daf7d168bc75692893939475b51a
han88bamboo <han@88bamboo.co>
2025-07-11 12:15:20 +0800

    Update be-drinkx-hash.txt

--- frontend/src/views/Producers/BottleListings.vue
+++ frontend/src/views/Producers/BottleListings.vue
@@ -2032,17 +2032,17 @@ tag, index
               <a v-if="isHttpValid(specified_listing['reviewLink'])" :href="specified_listing['reviewLink']"
                 class="text-left default-text-no-background row">
                 <div class="row">
-                  <div class="col-lg-4 col-md-6 col-sm-12">
+                  <!-- <div class="col-lg-4 col-md-6 col-sm-12">
                     {{ getOGImage(specified_listing["reviewLink"]) }}
-                    <!-- [if] there is a cover image for the post-->
+                     [if] there is a cover image for the post
                     <img v-if="ogImage != null" :src="ogImage[specified_listing.reviewLink]" alt="OG Image"
                       style="width: 80px; height: 80px" />
-                    <!-- [else] there is no cover image for the post (put 88 Bamboo's logo) -->
+                     [else] there is no cover image for the post (put 88 Bamboo's logo) 
                     <img v-else
                       src="https://88bamboo.co/cdn/shop/files/88B_New_Logo_-_white_face_transparent_background_180x.png?v=1655894111"
                       style="width: 80px; height: 80px" />
-                  </div>
-                  <div class="col-lg-8 col-md-12">
+                  </div> -->
+                  <div class="col-lg-12 col-md-12">
                     {{ deepDiveLinkFormatted }}
                   </div>
                 </div>
