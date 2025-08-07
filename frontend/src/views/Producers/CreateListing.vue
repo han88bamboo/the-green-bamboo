6fafdc0f06c17081a33409cde6757f14dc8460a0
han88bamboo <han@88bamboo.co>
2025-08-07 23:14:27 +0800

    Update CreateListing.vue

--- frontend/src/views/Producers/CreateListing.vue
+++ frontend/src/views/Producers/CreateListing.vue
@@ -4,20 +4,17 @@
     <NavBar />
     <SubmitListingNew formType="power" formMode="new" />
     
-    <FooterBar />
 </template>
 
 <script>
     import NavBar from '@/components/NavBar.vue';
     import SubmitListingNew from '@/components/SubmitListingNew.vue';
-    import FooterBar from "@/components/FooterBar.vue";
 
     export default {
         name: 'CreateListing',
         components: {
             NavBar,
-            SubmitListingNew,
-            FooterBar
+            SubmitListingNew
         },
     };
 </script>
