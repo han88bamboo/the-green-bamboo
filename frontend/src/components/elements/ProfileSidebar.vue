4a6c6728b02c1dded9860669bd5126bc322abf1d
MingSheng92 <cming0721@gmail.com>
2025-08-19 00:11:46 +0800

    WIP venue profile refac
    -resue eventbox component in venue profile

--- frontend/src/components/elements/ProfileSidebar.vue
+++ frontend/src/components/elements/ProfileSidebar.vue
@@ -235,24 +235,26 @@
         </div>
 
         <!-- Event Box -->
-        <div class="square primary-square-green-outline rounded p-3 mb-0">
+        <!-- <div class="square primary-square-green-outline rounded p-3 mb-0">
             <div class="d-flex justify-content-between align-items-center mb-0">
-                <h5 class="fw-bold">Upcoming Events</h5>
+                <h5 class="fw-bold">Your Events</h5>
                 <button v-if="isSelfView" class="btn btn-sm btn-outline-secondary">Edit</button>
             </div>
             <p v-if="loading" class="text-start">retrieving events ... </p>
             <p v-else class="text-start">{{ venue.events? venue.events : 'No events added yet.' }}</p>
-        </div>
+        </div> -->
+
+        <EventBox :selfView="isSelfView" :targetUserID="venue.id" targetUserType="venue" />
 
     </div>
 </template>
 
 <script>
-// import EventBox from '@/components/EventBox.vue'; // Assuming path
+import EventBox from '@/components/EventBox.vue'; // Assuming path
 
 export default {
     name: 'ProfileSidebar',
-    // components: { EventBox },
+    components: { EventBox },
     props: {
         loading: Boolean,
         error: String,
