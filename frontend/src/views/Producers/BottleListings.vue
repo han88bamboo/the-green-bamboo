ba2ff1baa53d3b5c4626efcb1310605cd4e5cbe7
han88bamboo <han@88bamboo.co>
2025-09-06 02:10:25 +0800

    update This is the Master Listing for all vintages.

--- frontend/src/views/Producers/BottleListings.vue
+++ frontend/src/views/Producers/BottleListings.vue
@@ -47,12 +47,12 @@
               d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
           </svg>
           <div class="text-start mobile-view-hide">
-            <span class="fw-bold text-dark">Master Listing for all vintages.</span> <span class="text-muted ms-2">For {{
+            <span class="fw-bold text-dark">This is the Master Listing for all vintages.</span><span class="text-muted ms-2">For {{
               specified_listing.drinkType ? `${specified_listing.drinkType} listings` : 'listings of this Drink Type'
               }}, specific vintages can be reviewed under "Add Your Review".</span>
           </div>
           <div class="text-start mobile-view-show fs-8">
-            <span class="fw-bold text-dark">Master Listing.</span><span class="text-muted ms-2">For {{
+            <span class="fw-bold text-dark">This is the Master Listing for all vintages.</span><span class="text-muted ms-2">For {{
               specified_listing.drinkType ? `${specified_listing.drinkType} listings` : 'listings of this Drink Type'
               }}, specific vintages can be reviewed in "Add Your Review".</span>
           </div>
