51e25b075550573155ae3560099a30f2eb15ca37
han88bamboo <han@88bamboo.co>
2025-09-08 15:30:23 +0800

    Update MyCellarPage.vue

--- frontend/src/views/MyCellarPage.vue
+++ frontend/src/views/MyCellarPage.vue
@@ -162,9 +162,7 @@
                       @click="viewMode = 'list'"
                       title="List View"
                     >
-                      <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                      </svg>
+                      <i class="bi bi-list-task"></i>
                     </button>
                     <button
                       type="button"
@@ -201,9 +199,7 @@
                       @click="viewMode = 'list'"
                       title="List View"
                     >
-                      <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                      </svg>
+                      <i class="bi bi-list-task"></i>
                     </button>
                     <button
                       type="button"
