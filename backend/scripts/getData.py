1438a05714549ae27bbcfb44dea643ed9ba0c5ec
han88bamboo <han@88bamboo.co>
2025-09-24 23:19:38 +0800

    Update getData.py

--- backend/scripts/getData.py
+++ backend/scripts/getData.py
@@ -400,6 +400,22 @@ def getListingsWithTags():
 
 
 # -----------------------------------------------------------------------------------------
+# [GET] Recent Listings (Past 48 Hours)
+@blueprint.route("/getRecentListings", methods=['GET'])
+def getRecentListings():
+    conn = g.db
+
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT * FROM "listings" WHERE "addedDate" >= NOW() - INTERVAL \'48 hours\'')
+        listings_data = cursor.fetchall()
+    
+    if not listings_data:
+        return jsonify([])
+
+    return jsonify(listings_data)
+
+
+# -----------------------------------------------------------------------------------------
 # [GET] Listings by user id
 @blueprint.route("/lbListings", methods=['GET'])
 def lbListings():
